a4e6c8dd6031fed27d336317c09fbab9efa4d802
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-08-14 23:28:26 +0800

    主题问题

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,120 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container style="height:100%">
+      <el-aside style="height:100%; border-right: solid 1px #F2F6FC;"> 
+        <div id="aside">
+          <div id="tungsten-title">@狼剩子</div>
+          <iframe src="https://ghbtns.com/github-btn.html?user=wolf-tungsten&type=follow&count=true" frameborder="0" scrolling="0" width="190px" height="20px"></iframe>
+          <div id="tungsten-avatar-box">
+            <img id="tungsten-avatar" src="./assets/avatar.jpg"/>
+            <div id="tungsten-avatar-note">
+              你的BUG像一条恶犬 🐶，撞乱我心弦
+            </div>
+          </div>
+          <div id="tungsten-brief">
+              <div class="tungsten-brief-item">
+                🎓 东南大学计算机科学与技术专业本科生
+              </div>
+              <div class="tungsten-brief-item">
+                🐵 东南大学小猴偷米工作室 2018 届站长
+              </div>
+              <div class="tungsten-brief-item">
+                📭 gaoruihao@wolf-tungsten.com
+              </div>
+              <div class="tungsten-brief-item">
+                👬 欢迎技术交流
+              </div>
+          </div>
+          <div id="tungsten-music">
+            <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=260 height=86 src="//music.163.com/outchain/player?type=2&id=25706282&auto=1&height=66"></iframe>
+          </div>
+        </div>
+      </el-aside>
+      <el-main style="height:100%">
+        <router-view></router-view>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
 <style>
+html{
+  height:100%;
+}
+body{
+  height: 100%;
+  margin:0;
+}
 #app {
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
+#aside{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#tungsten-title{
+  font-size: 36px;
+  margin-top: 40px;
+  margin-bottom : 10px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#tungsten-avatar-box{
+  width: 240px;
+  border-radius: 8px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #EBEEF5;
+  overflow: hidden;
+  margin-top: 20px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+#tungsten-avatar{
+  width: 240px;
+  height: 240px;
+  
+}
+#tungsten-avatar-note{
+  font-size: 14px;
+  margin: 5px 10px 15px 15px;
+  text-align: left;
+}
+#tungsten-music{
+  width: 260px;
+  height: 86px;
+  border-radius: 8px;
+  overflow: hidden;
+  margin-top:20px;
+}
+#tungsten-brief{
+  width: 240px;
+  margin-top: 30px; 
+  border-top: solid 1px #EBEEF5;
+}
+.tungsten-brief-item{
+  font-size: 14px;
+  text-align: left;
+  color:#909399;
+  border-bottom: solid 1px #EBEEF5;
+  margin-top: 10px;
+  padding-bottom: 10px; 
+}
+
+.player{
+  box-shadow: none;
 }
 </style>
---
+++ src/plugins/element.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import 'element-ui/lib/theme-chalk/display.css';
+import { Button, Row, Col, Container, Header, Aside, Main, Footer, PageHeader, Menu, MenuItem, MenuItemGroup, Submenu } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Footer)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(PageHeader)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Submenu)
+
