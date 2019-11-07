5aa439cd42b223d55816337aec7a270b5b1814b9
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-11-07 11:07:43 +0800

    修改lint错误

--- src/App-Mobile.vue
+++ src/App-Mobile.vue
@@ -5,11 +5,9 @@
 </template>
 
 <script>
-import MusicPlayer from './components/MusicPlayer'
+//import MusicPlayer from './components/MusicPlayer'
 export default {
-  name: 'app',
-  components: {
-  }
+  name: 'app'
 }
 </script>
 
